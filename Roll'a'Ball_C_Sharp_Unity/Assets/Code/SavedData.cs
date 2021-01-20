09f285c87784363a0f52e2c23db8d68df7c0d93a
ValeriaVicious <valeriavicious13@gmail.com>
2021-01-20 16:56:35 +0800

    Класс SaveData
    скелет для дальнейшей работы с обобщениями. Класс для сохранения прогресса игры.

---
+++ Roll'a'Ball_C_Sharp_Unity/Assets/Code/SavedData.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace GeekBrains
+{
+    public sealed class SavedData<T>
+    {
+        #region Fields
+
+        public int CountBonuses;
+        public T IdPlayer;
+
+        #endregion
+
+
+        #region Methods
+        #endregion
+    }
+}
