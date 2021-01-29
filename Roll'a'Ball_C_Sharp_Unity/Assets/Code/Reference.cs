881c788c3480bb77fcd801092f3dfe58ed59cdc7
ValeriaVicious <valeriavicious13@gmail.com>
2021-01-29 12:13:41 +0800

    Создан класс Reference

---
+++ Roll'a'Ball_C_Sharp_Unity/Assets/Code/Reference.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+namespace GeekBrains
+{
+    public class Reference
+    {
+        #region Fields
+
+        private Camera _mainCamera;
+        private PlayerBall _playerBall;
+
+        #endregion
+
+
+        #region Methods
+
+        public PlayerBall PlayerBall
+        {
+            get
+            {
+                if(_playerBall == null)
+                {
+                    var gameObject = Resources.Load<PlayerBall>(InteractiveObjects.PlayerTag);
+                    _playerBall = Object.Instantiate(gameObject);
+                }
+                return _playerBall;
+            }
+        }
+
+        public Camera MainCamera
+        {
+            get
+            {
+                if(_mainCamera == null)
+                {
+                    _mainCamera = Camera.main;
+                }
+                return _mainCamera;
+            }
+        }
+
+        #endregion
+    }
+}
